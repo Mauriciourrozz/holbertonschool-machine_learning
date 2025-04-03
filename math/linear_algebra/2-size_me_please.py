8007974cbbcbaccc15d7947f4d86bbc4d0bee649
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-04-03 20:26:22 -0300

    Function that return the shape of matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """
+    Function that return the shape of a matrix
+    """
     forma = []
     while isinstance(matrix, list):
         forma.append(len(matrix))
