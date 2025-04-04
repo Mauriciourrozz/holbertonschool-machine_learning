918b0b9dc40bb98ed715796253247bfe0b811093
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-04-03 22:38:32 -0300

    Concatenates 2 matrix

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This function concatenates 2 matrix
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    This function concatenates 2 matrix
+    """
+    return arr1 + arr2
