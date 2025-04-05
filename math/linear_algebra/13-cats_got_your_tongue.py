d4d8a9a90d956af032ec2abc3ce85cb8d8e6efee
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-04-05 19:09:24 -0300

    Function that concatenates two numpy arrays along a specified axis

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+This function concatenates two numpy arrays along a specified axis
+"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    This function concatenates two numpy arrays along a specified axis
+    """
+    return np.concatenate((mat1, mat2), axis=axis)
