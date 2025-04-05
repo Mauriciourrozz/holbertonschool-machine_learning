4359022cb1e66b98a6e1eb3c655bf4279526d33c
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-04-05 19:14:34 -0300

    Function that multiplies two arrays

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This function multiplies two numpy arrays
+"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    This function multiplies two numpy arrays
+    """
+    return np.matmul(mat1, mat2)
