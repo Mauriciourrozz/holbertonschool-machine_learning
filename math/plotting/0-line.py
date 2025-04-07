b178e3d950ffaf211a5d15b1f757e87884b21ade
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-04-07 20:33:00 -0300

    Function that plot y as line graph

---
+++ math/plotting/0-line.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Line Graph
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    """
+    This function plot y as a line graph
+    """
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(y, color="r")
+    plt.show()
