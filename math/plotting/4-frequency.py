d701f2e9ceae7e86b3aa27dd9f40ff8782ab86a4
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-04-08 11:58:37 -0300

    Plot a histogram of student scores for a project

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -22,5 +22,5 @@ def frequency():
     plt.xlim(0, 100)
     plt.xticks(np.arange(0, 110, 10))
     plt.ylim(0, 30)
-    plt.hist(student_grades, bins=range(10, 110, 10), edgecolor='black')
+    plt.hist(student_grades, bins=range(0, 110, 10), edgecolor='black')
     plt.show()
