79f4309382f0b7825a7f6d79a134c760224f85ad
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-04-12 14:58:01 -0300

    Function that calculates sigma

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+summation_i_squared(n)
+"""
+
+
+def summation_i_squared(n):
+    """
+    Function that calculates sigma
+    """
+    return sum([i ** 2 for i in range(1, n + 1)])
