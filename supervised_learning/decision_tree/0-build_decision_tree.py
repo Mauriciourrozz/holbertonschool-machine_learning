86adb8632a80d14f7b8e446878d359e0b717ee5b
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-04-15 11:34:45 -0300

    Add function that find the maximum of the depths of the nodes

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -7,7 +7,9 @@ import numpy as np
 
 
 class Node:
-
+    """
+    Represents an internal node in the tree, where a decision is made
+    """
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
         self.feature = feature
