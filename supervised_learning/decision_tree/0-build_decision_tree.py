b89ca7c1786c5ed108d47a16e3467c3d687e9cb7
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-04-15 11:38:49 -0300

    Add documentation in class and functions

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -43,6 +43,9 @@ class Leaf(Node):
         self.depth = depth
 
     def max_depth_below(self):
+        """
+        Function that find the maximum of the depths of the nodes
+        """
         return self.depth
 
 
