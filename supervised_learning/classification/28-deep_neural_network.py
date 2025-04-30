9098a2326a4384f5111ebe629af60e88dcef2ece
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-04-30 14:47:18 -0300

    Update the class DeepNeuralNetwork

--- supervised_learning/classification/28-deep_neural_network.py
+++ supervised_learning/classification/28-deep_neural_network.py
@@ -31,7 +31,7 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if activation != 'sig' or activation != 'tanh':
+        if activation != 'sig' and activation != 'tanh':
             raise ValueError("activation must be 'sig' or 'tanh'")
 
         # número de características de entrada
