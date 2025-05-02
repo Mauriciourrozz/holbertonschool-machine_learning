dcee727fa410bd2359f3e9f94578d8ae6f4cf440
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-05-02 16:55:45 -0300

    Create model

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-import tensorflow.keras as k
+import tensorflow.keras as K
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
 
     # se cargan los datos de entrada al modelo y se guardan en inputs
-    inputs =  k.layers.Input(shape=(nx,))
+    inputs =  K.layers.Input(shape=(nx,))
 
     # se crea una capa densa y al poner (inputs) se conecta esa capa con la entrada
-    x = k.layers.Dense(units=layers[0], activation=activations[0], kernel_regularizer=k.regularizers.l2(lambtha))(inputs)
+    x = K.layers.Dense(units=layers[0], activation=activations[0], kernel_regularizer=K.regularizers.l2(lambtha))(inputs)
 
     # se recorren las capaz intermedias, la primera ya fue creada antes
     # y la ultima se hace despues
@@ -15,16 +15,16 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
         # se crea una nueva capa en cada iteracion y se conecta con la anterior
         # que fue x
-        x = k.layers.Dense(units=layers[i], activation=activations[i], kernel_regularizer=k.regularizers.l2(lambtha))(x)
+        x = K.layers.Dense(units=layers[i], activation=activations[i], kernel_regularizer=K.regularizers.l2(lambtha))(x)
 
         # dropout apaga neuronas al azar antes de seguir para que
         # la red no se apoye demasiado en ciertas neuronas
-        x = k.layers.Dropout(1 - keep_prob)(x)
+        x = K.layers.Dropout(1 - keep_prob)(x)
 
     # ultima capa, procesa los ultimos valores y se conecta a la
     # ultima capa intermedia
     output = x
     # se esta  creando el modelo completo, conectando todas las capas
-    model = k.Model(inputs=inputs, outputs=output)
+    model = K.Model(inputs=inputs, outputs=output)
 
     return model
