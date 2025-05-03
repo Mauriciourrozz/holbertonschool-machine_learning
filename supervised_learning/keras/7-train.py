6c69b8f686e3b22dad05140b1123c1ae1e7b629e
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-05-03 14:36:54 -0300

    Update the function train_model

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -50,7 +50,7 @@ def train_model(network, data, labels, batch_size, epochs,
         decaimiento = K.callbacks.LearningRateScheduler(
             lambda epochs: alpha / (1 + decay_rate * epochs),
             verbose=1)
-    callbacks.append(decaimiento)
+        callbacks.append(decaimiento)
 
     history = network.fit(data,
                           labels,
