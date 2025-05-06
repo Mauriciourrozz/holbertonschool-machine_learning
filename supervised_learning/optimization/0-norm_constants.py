09a375d8075e5d065b4b87246ef634eab7e27001
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-05-06 13:39:29 -0300

    Function that calculate the normalization constants

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -5,33 +5,18 @@
 import numpy as np
 
 
-def constantes_normalizacion(X):
+def normalization_constants(X):
     """
     Calculate the normalization constants (mean and standard deviation)
-    of each characteristic in the matrix.
+    of each characteristic in the matrix
 
     Parameters:
-    X (list of lists): Data array (m, nx)
+    X (numpy.ndarray): Shape data array (m, nx)
 
     Returns:
     tuple: mean and standard deviation of each feature (nx,)
     """
-    m = len(X)
-    nx = len(X[0])
-
-    medias = [0] * nx
-    desviaciones = [0] * nx
-
-    for j in range(nx):
-        suma_columna = 0
-        for i in range(m):
-            suma_columna += X[i][j]
-        medias[j] = suma_columna / m
-
-    for j in range(nx):
-        suma_varianza = 0
-        for i in range(m):
-            suma_varianza += (X[i][j] - medias[j]) ** 2
-        desviaciones[j] = (suma_varianza / m) ** 0.5
-
-    return medias, desviaciones
+    mean = X.mean(axis=0)
+    std = X.std(axis=0)
+    return mean, std
+    
