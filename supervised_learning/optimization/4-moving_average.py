8cf85f5b92405160aca4025be465a9236be9f0d5
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-05-09 11:32:32 -0300

    Function that calculate the expotencially weighted moving average

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -2,14 +2,16 @@
 """
 4-moving_average.py
 """
+
+
 def moving_average(data, beta):
     """
-    Calculate the exponentially weighted moving average (EMA) of a series of data.
-    
+    Calculate the exponentially weighted moving average of a series of data.
+
     Parameters:
     data (list or numpy.ndarray): A list of numerical values.
     beta (float): Smoothing factor, a value between 0 and 1.
-    
+
     Returns:
     list: The exponentially weighted moving average of the input data.
     """
@@ -21,5 +23,5 @@ def moving_average(data, beta):
         v = beta * v + (1 - beta) * value
         corrected_v = v / (1 - beta**(t + 1))
         moving_averages.append(corrected_v)
-    
+
     return moving_averages
