e175e483de5bf825fc0287a2074cabd41fad4035
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-05-11 19:45:04 -0300

    Creates a RMSProp optimizer in tensorflow

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+8-RMSProp.py
+"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """
+    Creates an RMSProp optimizer in TensorFlow.
+
+    Parameters:
+    alpha (float): Learning rate.
+    beta2 (float): Decay rate (rho) for the moving average of squared gradients.
+    epsilon (float): Small constant to prevent division by zero.
+
+    Returns:
+    tf.keras.optimizers.Optimizer: A TensorFlow RMSprop optimizer.
+    """
+    return tf.keras.optimizers.RMSprop(
+        learning_rate=alpha,
+        rho=beta2,
+        epsilon=epsilon)
