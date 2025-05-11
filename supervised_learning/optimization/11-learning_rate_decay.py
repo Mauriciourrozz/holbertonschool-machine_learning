8d39335509ca6b30ec81cf7a0aac0d264ea4e112
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-05-11 20:54:12 -0300

    Updates the learning rate using inverse time decay

---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    Updates the learning rate using inverse time decay.
+
+    Parameters:
+    alpha (float): Initial learning rate.
+    decay_rate (float): The rate at which alpha decays.
+    global_step (int): Number of gradient descent steps taken.
+    decay_step (int): Number of steps before applying further decay.
+
+    Returns:
+    float: The updated learning rate.
+    """
+    return alpha / (1 + decay_rate * (global_step // decay_step))
