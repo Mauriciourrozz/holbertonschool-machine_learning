d76fc5e5222634068d03f90ddedbd4f504e5924f
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-05-11 21:20:47 -0300

    Creates a batch normalization layer for a neural network in TensorFlow

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -12,7 +12,8 @@ def create_batch_norm_layer(prev, n, activation):
     Parameters:
     prev (tf.Tensor): Activated output of the previous layer.
     n (int): Number of nodes in the new layer.
-    activation (function): Activation function to apply after batch normalization.
+    activation (function): Activation function to apply after batch
+    normalization.
 
     Returns:
     tf.Tensor: Activated output of the batch-normalized layer.
@@ -25,7 +26,10 @@ def create_batch_norm_layer(prev, n, activation):
     gamma = tf.Variable(tf.constant(1.0, shape=[n]))
     beta = tf.Variable(tf.constant(0.0, shape=[n]))
     mean, variance = tf.nn.moments(layer, axes=[0])
-    bn = tf.nn.batch_normalization(layer, mean=mean, variance=variance,
-                                    offset=beta, scale=gamma,
-                                    variance_epsilon=1e-7)
+    bn = tf.nn.batch_normalization(layer,
+                                   mean=mean,
+                                   variance=variance,
+                                   offset=beta,
+                                   scale=gamma,
+                                   variance_epsilon=1e-7)
     return activation(bn)
