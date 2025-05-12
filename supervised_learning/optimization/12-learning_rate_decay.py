df35d89768c53e7950b311769c4ec2ed3316dc1b
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-05-11 21:02:45 -0300

    Creates a learning rate decay operation using inverse time decay

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -15,7 +15,8 @@ def learning_rate_decay(alpha, decay_rate, decay_step):
     decay_step (int): Number of steps before applying more decay.
 
     Returns:
-    tf.keras.optimizers.schedules.LearningRateSchedule: A learning rate schedule.
+    tf.keras.optimizers.schedules.LearningRateSchedule: A learning
+    rate schedule.
     """
     return tf.keras.optimizers.schedules.InverseTimeDecay(
         initial_learning_rate=alpha,
