5c81e04aa71b2809cf9bd21495090a9d3382ce98
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-05-13 11:03:13 -0300

    Calculates the precision for each class in a confusion matrix

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+2-precision.py
+"""
+import numpy as np
+
+
+def precision(confusion):
+    """
+    Calculates the precision for each class in a confusion matrix.
+
+    Arguments:
+    confusion(numpy.ndarray): Confusion matrix of shape (n_classes, n_classes),
+    where confusion[i, j] represents the number of elements from class i
+    predicted as class j.
+
+    Returns:
+    numpy.ndarray: An array of shape (n_classes,) containing the precision
+    for each class.
+    """
+    precisiones = np.zeros(confusion.shape[0])
+    for i in range(confusion.shape[0]):
+        vp = confusion[i, i]
+        fp = np.sum(confusion[:, i]) - confusion[i, i]
+        precision = vp / (vp + fp)
+        precisiones[i] = precision
+    return precisiones
