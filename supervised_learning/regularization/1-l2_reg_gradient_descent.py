07f7736304fce96830b0d58d6f7db729aaecf8ae
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-05-16 10:53:11 -0300

    Updates the weights and biases of a neural network using gradient descent with L2 regularization

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -33,22 +33,21 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         This function updates the weights dictionary **in-place**.
     """
     m = Y.shape[1]
+    dZ = 0
 
-    dZ = cache['A' + str(L)] - Y
+    for l in reversed(range(1, L + 1)):
+        A_curr = cache['A' + str(l)]
+        A_prev = cache['A' + str(l - 1)]
+        W_curr = weights['W' + str(l)]
+        b_curr = weights['b' + str(l)]
 
-    for i in reversed(range(1, L + 1)):
-        A_prev = cache['A' + str(i - 1)]
-        W = weights['W' + str(i)]
-        b = weights['b' + str(i)]
+        if l == L:
+            dZ = A_curr - Y
+        else:
+            dZ = np.dot(weights['W' + str(l + 1)].T, dZ) * (1 - A_curr ** 2)
 
-        dW = (1 / m) * (dZ @ A_prev.T) + (lambtha / m) * W
+        dW = (1 / m) * np.dot(dZ, A_prev.T) + (lambtha / m) * W_curr
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        db = (1 / m) * dZ.sum(axis=1, keepdims=True)
-
-        weights['W' + str(i)] = W - alpha * dW
-        weights['b' + str(i)] = b - alpha * db
-
-        if i > 1:
-            A_prev = cache['A' + str(i - 1)]
-            W = weights['W' + str(i)]
-            dZ = (W.T @ dZ) * (1 - A_prev ** 2)
+        weights['W' + str(l)] -= alpha * dW
+        weights['b' + str(l)] -= alpha * db
