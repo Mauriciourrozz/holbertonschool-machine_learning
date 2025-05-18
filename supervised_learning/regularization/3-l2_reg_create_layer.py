74b8410b13772de76178a6f83d9db4025ed468ed
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-05-17 22:17:14 -0300

    Create a neural network layer with L2 regularization

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -14,7 +14,10 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     activation: activation function
     lambtha: L2 regularization lambda
     """
-    l2 = tf.keras.regularizers.L2(lambtha)
-    layer = tf.keras.layers.Dense(units=n, activation=activation,
-                                  kernel_regularizer=l2)(prev)
+    initializer = tf.keras.initializers.VarianceScaling(scale=2.0, mode='fan_avg')
+    regularizer = tf.keras.regularizers.L2(lambtha)
+    layer = tf.keras.layers.Dense(units=n,
+                                  activation=activation,
+                                  kernel_initializer=initializer,
+                                  kernel_regularizer=regularizer)(prev)
     return layer
