fd92afd089fd672af0b1cb554e3bb78a1b87c929
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-05-18 22:47:02 -0300

    Updates the weights of a dropout-regularized neural network using gradient descent

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -20,26 +20,20 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     L: total number of layers.
     """
     m = Y.shape[1]
+    A_output = cache['A' + str(L)]
+    dZ = A_output - Y
+    grads = {}
 
-    A_final = cache['A' + str(L)]
+    for i in range(L, 0, -1):
+        A_prev = cache['A' + str(i-1)]
+        grads['dW' + str(i)] = np.dot(dZ, A_prev.T) / m
+        grads['db' + str(i)] = np.sum(dZ, axis=1, keepdims=True) / m
 
-    dZ = A_final - Y
-
-    for layer in range(L, 0, -1):
-        A_prev = cache['A' + str(layer - 1)]
-
-        dW = np.dot(dZ, A_prev.T) / m
-
-        db = np.sum(dZ, axis=1, keepdims=True) / m
-
-        weights['W' + str(layer)] = weights['W' + str(layer)] - alpha * dW
-        weights['b' + str(layer)] = weights['b' + str(layer)] - alpha * db
-
-        if layer > 1:
-            dA = np.dot(weights['W' + str(layer)].T, dZ)
-
-            D = cache['D' + str(layer - 1)]
-            dA = dA * D
-            dA = dA / keep_prob
-
+        if i > 1:
+            dA = np.matmul(weights['W' + str(i)].T, dZ)
+            dA *= cache['D' + str(i-1)]
+            dA /= keep_prob
             dZ = dA * (1 - np.power(A_prev, 2))
+
+        weights['W' + str(i)] -= alpha * grads['dW' + str(i)]
+        weights['b' + str(i)] -= alpha * grads['db' + str(i)]
