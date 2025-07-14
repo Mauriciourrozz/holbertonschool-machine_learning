0e65c8bc2fcfc094f73f2538be5212c548c4e7e5
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-07-14 18:43:52 -0300

    Calculates the matrix of minors of a square matrix

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -76,10 +76,13 @@ def minor(matrix):
     """
     if not matrix:
         raise TypeError("matrix must be a list of lists")
+
     if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
+
     if not matrix or matrix == [] or matrix == [[]] or len(matrix) == 0:
         raise ValueError("matrix must be a non-empty square matrix")
+
     if not all(len(row) == len(matrix) for row in matrix):
         raise ValueError("matrix must be a non-empty square matrix")
 
