d14697801a4c65f6a3bbb1ed7e606469436dce53
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-07-31 16:12:21 -0300

    Initializate pi, m and S to GMM

---
+++ unsupervised_learning/clustering/4-initialize.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+4-initialize.py
+"""
+import numpy  as np
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """
+    Initializate pi, m and S to GMM
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None, None
+    if type(k) is not int or k <= 0 or k >= X.shape[0]:
+        return None, None, None
+
+    pi = np.full((k,), 1 / k)
+    m, _ = kmeans(X, k)
+    d = X.shape[1]
+    S = np.array([np.identity(d) for _ in range(k)])
+
+    return pi, m, S
