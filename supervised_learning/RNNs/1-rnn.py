7d9306d64e2211e2affb33f4ee5bed4348c729ea
Mauriciourrozz <mauriciourrozz@gmail.com>
2025-09-06 00:42:53 +0000

    Perform forward propagation for a simple RNN

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -18,28 +18,13 @@ def rnn(rnn_cell, X, h_0):
     H -- numpy.ndarray of shape (t, m, h), all hidden states
     Y -- numpy.ndarray of shape (t, m, o), all outputs
     """
-    # Número de pasos de tiempo (t), tamaño del lote (m), y dimensión de entrada (i)
     t, m, i = X.shape
-    h = h_0.shape[1]
-    
-    # para guardar todos los estados ocultos
-    H = np.zeros((t, m, h))
-    # para guardar todas las salidas
-    Y = np.zeros((t, m, rnn_cell.Wy.shape[1]))
-    
-    # Establecer el estado oculto inicial
+    h = np.zeros((t, m, rnn_cell.Wh.shape[1]))
+    y = np.zeros((t, m, rnn_cell.Wy.shape[1]))
     h_prev = h_0
-    
-    # Iteramos sobre los pasos de tiempo
-    for step in range(t):
-        # Obtención de la entrada en el tiempo t
-        x_t = X[step]
-        
-        # Propagación hacia adelante usando la celda RNN
-        h_prev, y_t = rnn_cell.forward(h_prev, x_t)
-        
-        # Guardamos el estado oculto y la salida en sus respectivos arrays
-        H[step] = h_prev
-        Y[step] = y_t
-    
-    return H, Y
+
+    for time_step in range(t):
+        h[time_step], y[time_step] = rnn_cell.forward(h_prev, X[time_step])
+        h_prev = h[time_step]
+
+    return h, y
