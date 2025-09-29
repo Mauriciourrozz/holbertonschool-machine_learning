2db9ec3ac890b5348e4e767d843b893880d7511b
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-09-29 11:01:28 -0300

    Create a class Encoder

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-9-transformer_encoder.py
+9- transformer_encoder.py
 """
 import tensorflow as tf
 positional_encoding = __import__('4-positional_encoding').positional_encoding
@@ -13,6 +13,9 @@ class Encoder(tf.keras.layers.Layer):
     """
     def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
                  drop_rate=0.1):
+        """
+        class constructor
+        """
         super(Encoder, self).__init__()
         self.N = N
         self.dm = dm
@@ -20,7 +23,7 @@ class Encoder(tf.keras.layers.Layer):
         self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
         # Positional encoding directamente usando la función importada
         self.positional_encoding = positional_encoding(max_seq_len, dm)
-        # Lista de EncoderBlocks
+        # Lista de Encoder Blocks
         self.blocks = [
             EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
         # Dropout aplicado a los embeddings con posicion
