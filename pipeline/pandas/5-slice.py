b6f011987e198cabaae079fe3515244275d3bb30
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-11-19 17:04:04 +0000

    Extracts the High, Low, Close, and Volume_BTC columns, selects every 60th row, and returns the sliced DataFrame

---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+5-slice.py
+"""
+
+
+def slice(df):
+    """
+    Extracts the High, Low, Close, and Volume_BTC columns,
+    selects every 60th row, and returns the sliced DataFrame.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing the required columns.
+
+    Returns:
+        pd.DataFrame: The sliced DataFrame.
+    """
+    # Quedarse solo con las columnas necesarias
+    sub = df[['High', 'Low', 'Close', 'Volume_BTC']]
+
+    # Seleccionar cada fila número 60
+    return sub.iloc[::60]
