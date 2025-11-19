24ec25e1005f69370f33418cc6c5d2c706e4279a
Mauriciourrozz <Mauriciourrozz@gmail.com>
2025-11-19 16:53:07 +0000

    Renames the 'Timestamp' column to 'Datetime', converts it to datetime, and returns a DataFrame containing only the 'Datetime' and 'Close' columns

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+3-rename.py
+"""
+import pandas as pd
+
+
+def rename(df):
+    """
+    Renames the 'Timestamp' column to 'Datetime', converts it to datetime,
+    and returns a DataFrame containing only the 'Datetime' and 'Close' columns.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing a column named 'Timestamp'.
+
+    Returns:
+        pd.DataFrame: The modified DataFrame with 'Datetime' and 'Close'
+        columns.
+    """
+    # Cambiar nombre de columna
+    df = df.rename(columns={'Timestamp': 'Datetime'})
+
+    # Convertir a datetime
+    df['Datetime'] = pd.to_datetime(df['Datetime'])
+
+    # Mostrar solo estas dos columnas
+    df = df[['Datetime', 'Close']]
+
+    return df
