8cb27c20bbaf606666cd8ad6db9a8c251ed1d35a
Mauriciourrozz <mauriciourrozz@gmail.com>
2025-11-21 19:43:22 +0000

    Script that displays the first SpaceX launch

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -3,43 +3,38 @@
 Script that displays the first SpaceX launch
 """
 
-import requests
-from datetime import datetime
 
 if __name__ == "__main__":
-    # Urls principales de la API no oficial de SpaceX
+    # Endpoint de lanzamientos
     launches_url = "https://api.spacexdata.com/v4/launches"
-    rockets_url = "https://api.spacexdata.com/v4/rockets/"
-    launchpads_url = "https://api.spacexdata.com/v4/launchpads/"
 
-    # Obtener todos los lanzamientos
     launches = requests.get(launches_url).json()
 
-    # Ordenar por date_unix en orden ascendente
-    launches = sorted(
-        launches,
-        key=lambda x: x.get("date_unix", float("inf"))
-    )
+    # Ordenar por date_unix (segundos)
+    launches_sorted = sorted(launches, key=lambda x: x.get("date_unix", 0))
 
-    # Tomar el primer lanzamiento
-    first = launches[0]
+    # Primer lanzamiento
+    first = launches_sorted[0]
 
-    launch_name = first.get("name")
-    date_unix = first.get("date_unix")
-    rocket_id = first.get("rocket")
-    launchpad_id = first.get("launchpad")
+    # Obtener datos individuales
+    launch_name = first["name"]
 
-    # Convertir fecha a hora local
-    date_local = datetime.fromtimestamp(date_unix).strftime("%Y-%m-%d %H:%M:%S")
+    # Convertimos fecha a local
+    date_local = datetime.fromtimestamp(
+        first["date_unix"]).strftime("%Y-%m-%d %H:%M:%S")
 
-    # Obtener datos del cohete
-    rocket = requests.get(rockets_url + rocket_id).json()
-    rocket_name = rocket.get("name")
+    # Obtener detalles del cohete
+    rocket_id = first["rocket"]
+    rocket = requests.get(
+        f"https://api.spacexdata.com/v4/rockets/{rocket_id}").json()
+    rocket_name = rocket["name"]
 
-    # Obtener datos de la plataforma
-    launchpad = requests.get(launchpads_url + launchpad_id).json()
-    launchpad_name = launchpad.get("name")
-    launchpad_locality = launchpad.get("locality")
+    # Obtener detalles del launchpad
+    pad_id = first["launchpad"]
+    pad = requests.get(
+        f"https://api.spacexdata.com/v4/launchpads/{pad_id}").json()
+    pad_name = pad["name"]
+    pad_locality = pad["locality"]
 
-    # Imprimir
-    print(f"{launch_name} ({date_local}) {rocket_name} - {launchpad_name} ({launchpad_locality})")
+    # Formato de salida
+    print(f"{launch_name} ({date_local}) {rocket_name} - {pad_name} ({pad_locality})")
