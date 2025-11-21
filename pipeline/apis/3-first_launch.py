60e076c5ed488011405c9c8707146cac8799c9ec
Mauriciourrozz <mauriciourrozz@gmail.com>
2025-11-21 19:51:11 +0000

    Script that displays the first SpaceX launch

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -5,18 +5,23 @@ Script that displays the first SpaceX launch
 
 
 def get_json(url):
+    """
+    Get data from a URL and return the result as JSON
+    """
     return requests.get(url).json()
+
 
 if __name__ == "__main__":
     # Obtener todos los lanzamientos próximos
     upcoming = get_json("https://api.spacexdata.com/v4/launches/upcoming")
 
     # Ordenar por fecha usando date_unix
-    first_launch = sorted(upcoming, key=lambda l: l["date_unix"])[0]
+    first_launch = sorted(upcoming, key=lambda i: i["date_unix"])[0]
 
     # Obtener datos del cohete
     rocket_id = first_launch["rocket"]
-    rocket_info = get_json(f"https://api.spacexdata.com/v4/rockets/{rocket_id}")
+    rocket_info = get_json(
+        f"https://api.spacexdata.com/v4/rockets/{rocket_id}")
 
     # Obtener datos de la plataforma
     pad_id = first_launch["launchpad"]
