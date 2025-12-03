5e886712f4244bf2c91968366b5267d241121d14
Mauriciourrozz <mauriciourrozz@gmail.com>
2025-12-03 20:52:06 +0000

    Function SafeDiv, safely divides a by b, returns 0 if b is 0

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,17 @@
+-- Function SafeDiv, safely divides a by b, returns 0 if b is 0
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    -- Si el divisor es 0, devolver 0
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+
+    -- Si no, devolver a / b
+    RETURN a / b;
+END$$
+
+DELIMITER ;
