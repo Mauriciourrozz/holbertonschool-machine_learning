73b378e7e535711dc498e0edef492aae7b1dec47
Mauriciourrozz <mauriciourrozz@gmail.com>
2025-12-04 15:45:36 +0000

    Insert a new document in the collection using kwargs

---
+++ pipeline/databases/31-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+31-insert_school.py
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Insert a new document in the collection using kwargs.
+    Returns the inserted document _id.
+    """
+    # Inserta el documento usando los kwargs
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
